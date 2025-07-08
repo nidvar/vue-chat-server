f1e6cb057171d5abe491740ac65c472bbaf93bc7
Nidvar <djayshine87@hotmail.com>
2025-07-09 08:44:27 +1000

    Update server.js

--- server.js
+++ server.js
@@ -10,9 +10,19 @@ connectDB();
 
 const app = express();
 
+const allowedOrigins = ['https://mevn-blog.vercel.app'];
+
 app.use(cors({
-  origin: process.env.FRONTEND_URL,
-  credentials: true
+  origin: function (origin, callback) {
+    // Allow requests with no origin (like mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    } else {
+      return callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true, // if you're using cookies/auth headers
 }));
 
 app.use(cookieParser());
