7ba5a2f2641ee8fd05d03fb956b3c2de4f998ae6
Nidvar <djayshine87@hotmail.com>
2025-07-09 09:20:30 +1000

    Update main.js

--- routes/main.js
+++ routes/main.js
@@ -159,13 +159,6 @@ export const routes = function(app){
                   httpOnly: true,
                   secure: true,
                   sameSite: 'None',
-                  maxAge: 1000 * 60 * 60 * 24, // 1 day (optional)
-                });
-
-                res.cookie('token', token, {
-                  httpOnly: true,
-                  secure: true,
-                  sameSite: 'None',
                   maxAge: 1000 * 60 * 60 * 24,
                 });
                 
